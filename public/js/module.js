b09e78654c7ebb3095bdff624a4ae0ff011f9394
alexwebdev <aleksander.soja@gmail.com>
2015-06-23 21:01:02 +0200

    Get data ctrl

--- public/js/module.js
+++ public/js/module.js
@@ -1,1 +1,10 @@
-angular.module('juneApp', []);
+angular.module('juneApp', [])
+.constant('baseUrl', 'http://localhost:2403/products/')
+.controller('mainCtrl', function($scope, $http, baseUrl) {
+
+	$http.get(baseUrl).success(function(data) {
+		$scope.products = data;
+	});
+
+
+});
